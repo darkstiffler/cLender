3c015f79ee53d433cffa670a790fa3b488dcb924
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-08-13 13:47:40 -0700

    add functionality to user resource

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,29 @@
 class UsersController < ApplicationController
-	
-	def index
-	end
+def index
+    @users = User.all
+  end
 
-	def create
-	end
+  # Get /users/:id - display detail for user <id>
+  def show
+    @user = User.find params[:id]
+  end
 
-	def destroy
-	end	
-	
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def create
+    @user = User.create user_params
+
+    # this will redirect the user    
+     if @user
+      redirect_to users_path, status: 303
+    else
+      render :new
+    end
+  end
 end
