459d30962a846f7146f7ecd54060fbca7a11c23b
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-08-20 11:27:39 -0700

    edit routes css and html

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 CLender::Application.routes.draw do
 
+  # defined GET routes and there values
   get "cars/index"
   get "cars/create"
   get "cars/new"
   get "cars/show"
   get "cars/update"
   get "cars/destroy"
-  
+  get '/login' => 'users#login'
+
+  # site root {where the page goes when it loads up localhost:x }
   root "site#index"
 
+  # resources {this creates all 7 methods tied to that name}
   resources :users
   resources :cars
   resources :car_make
   resources :car_model
 
-  get '/login' => 'users#login'
 end
